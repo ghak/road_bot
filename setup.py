0b86f99df08a67be8ef31b77376bbda40d25e0a3
ghak <hakimguedjou@gmail.com>
2023-01-14 08:04:41 +0100

    Adding launch file for joystick and image streaming

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+import glob
 
 package_name = 'road_bot'
 
@@ -7,9 +9,10 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob.glob('launch/*launch.[pxy][yma]*'))
+        
     ],
     install_requires=['setuptools'],
     zip_safe=True,
